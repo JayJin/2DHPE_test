0fef28ad94c4a48db5ce2b6e643f25944ab91e8a
JayJin <jvidorca@gmail.com>
2023-10-09 13:58:26 +0900

    Add setup for extraction interval time(fps setup)

--- PoseDetection.py
+++ PoseDetection.py
@@ -24,6 +24,7 @@ clm_list = [
 def get_landmarks(video_route):
     print(video_route)
     cap = cv2.VideoCapture(video_route)
+
     mp_pose = mp.solutions.pose
     mp_draw = mp.solutions.drawing_utils
     mp_drawing_styles = mp.solutions.drawing_styles
@@ -37,44 +38,46 @@ def get_landmarks(video_route):
 
     title = video_route.split('/')[-1].split('.')[0]
 
+    # 랜드마크 추출 인터벌 설정
+    extract_interval_seconds = 0.1
+    extract_interval_frames = int(cap.get(cv2.CAP_PROP_FPS) * extract_interval_seconds)
+
+    frame_count = 0
+
     with mp_pose.Pose(
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as pose:
         while True:
             ret, img = cap.read()
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img = cv2.resize(img, (200, 400))
-            results = pose.process(img)
+            if not ret:
+                break
+
+            frame_count += 1
+
+            if frame_count % extract_interval_frames == 0:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                img = cv2.resize(img, (200, 400))
+                results = pose.process(img)
+
+                # 랜드마크 생성
+                if results.pose_landmarks:
+                    mp_draw.draw_landmarks(img, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+                    x = []
+                    for k in range(33):
+                        if (11 <= k < 17) or (23 <= k < 29):
+                            x.append(results.pose_landmarks.landmark[k].x)
+                            x.append(results.pose_landmarks.landmark[k].y)
+                            x.append(results.pose_landmarks.landmark[k].z)
+                            x.append(results.pose_landmarks.landmark[k].visibility)
+
+                    # list x를 dataframe으로 변경하여 정보 쌓기(33개 landmarks의 (33*4, x y z, vis) 132개 정보)
+                    tmp = pd.DataFrame(x).T
+                    df = pd.concat([df, tmp])
+                    df.to_csv(f"./data/landmarks/{title}.csv", index=False, header=False)
             
-            # 랜드마크 생성
-            if results.pose_landmarks:
-                mp_draw.draw_landmarks(img, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-                for id, lm in enumerate(results.pose_landmarks.landmark):
-                    h, w, c = img.shape
-                    # print(id, lm)
+                cv2.imshow("Estimation", img)
+                cv2.waitKey(1)
 
-            cv2.imshow("Estimation", img)
-
-            # 빈 리스트 x 생성
-            x = []      
-
-            for k in range(33):
-                if (11 <= k < 17) | (23 <= k <29):
-                    x.append(results.pose_landmarks.landmark[k].x)
-                    x.append(results.pose_landmarks.landmark[k].y)
-                    x.append(results.pose_landmarks.landmark[k].z)
-                    x.append(results.pose_landmarks.landmark[k].visibility)
-
-            # list x를 dataframe으로 변경
-            tmp = pd.DataFrame(x).T
-
-            # dataframe에 정보 쌓기(33개 landmarks의 (33*4, x y z, vis)132개 정보)
-            df = pd.concat([df, tmp])
-            df.to_csv(f"./data/landmarks/{title}.csv", index=False, header=False)
-            cv2.waitKey(1)
-
-
-
-
-
+    cap.release()
+    cv2.destroyAllWindows()
