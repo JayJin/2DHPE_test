42eb1f99169e15f17f8336e497c9415f270ad235
JayJin <jvidorca@gmail.com>
2023-10-09 18:33:55 +0900

    Add Cosine Similarity

--- comparing_landmarks.py
+++ comparing_landmarks.py
@@ -14,25 +14,25 @@ def Compare(lpath_1, lpath_2, sync_difference_seconds):
             data = list(csv.reader(f, delimiter=","))
         ldmk_1 = np.array(data)[1+diff:, 1:]
         r1, c1 = ldmk_1.shape
-        print('r1, c1:', r1, c1)
+        # print('r1, c1:', r1, c1)
 
         with open(lpath_2, 'r') as f:
             data = list(csv.reader(f, delimiter=","))
         ldmk_2 = np.array(data)[1:, 1:]
         r2, c2 = ldmk_2.shape
-        print('r2, c2:', r2, c2)
+        # print('r2, c2:', r2, c2)
     else:
         with open(lpath_1, 'r') as f:
             data1 = list(csv.reader(f, delimiter=","))
         ldmk_1 = np.array(data1)[1:, 1:]
         r1, c1 = ldmk_1.shape
-        print('r1, c1:', r1, c1)
+        # print('r1, c1:', r1, c1)
 
         with open(lpath_2, 'r') as f:
             data2 = list(csv.reader(f, delimiter=","))
         ldmk_2 = np.array(data2)[1+(diff*(-1)+1):, 1:]
         r2, c2 = ldmk_2.shape
-        print('r2, c2:', r2, c2)
+        # print('r2, c2:', r2, c2)
 
     length_val = min(r1, r2)
     ldmk_1 = ldmk_1[:length_val]
@@ -41,21 +41,13 @@ def Compare(lpath_1, lpath_2, sync_difference_seconds):
     # normalize
     ldmk_1_normalized_l2 = preprocessing.normalize(ldmk_1, norm = 'l2')
     ldmk_2_normalized_l2 = preprocessing.normalize(ldmk_2, norm = 'l2')
-    print(ldmk_1_normalized_l2)
-    print(ldmk_2_normalized_l2)
 
-    print("한 행씩 출력")
-    print(ldmk_1_normalized_l2[0])
-    print(ldmk_2_normalized_l2[0])
-
-    print("코사인 유사도 계산")
-    cosine_sim = cosine_similarity(ldmk_1_normalized_l2[0], ldmk_2_normalized_l2[0])
-    print(cosine_sim.shape)
-    print(cosine_sim)
-    # # Cosine Similarity
-    # for i in range(1, length_val):
-    #     cosine_sim = cosine_similarity(ldmk_1_normalized_l2[i], ldmk_2_normalized_l2[i])
-    #     print('코사인 유사도 연산 결과 :',cosine_sim)
+    # Cosine Similarity
+    cosine_sim = cosine_similarity(ldmk_1_normalized_l2, ldmk_2_normalized_l2)
+    results = np.diag(cosine_sim)
+    mean_score = np.mean(results)
+    print(str(round(mean_score*100, 1))+"점")
+    return results, mean_score
 
 
 
@@ -66,10 +58,7 @@ vpath_1 = ".\data\\videos\\202310091700_스모크챌린지.mp4"
 vpath_2 = ".\data\\videos\\202310091701_스모크챌린지 Choreo by 바다.mp4"
 
 
-# ★ TEST 확인 완료시 복구해야 함!!!!!!!   - Cross Cor 동작 시간으로 현재 주석처리함
-# sync = MatchSync(vpath_1, vpath_2)
-# print(math.ceil(sync * 10))
-# Compare(lpath_1, lpath_2, math.ceil(sync * 10))
+sync = MatchSync(vpath_1, vpath_2)
+print(math.ceil(sync * 10))
+Compare(lpath_1, lpath_2, math.ceil(sync * 10))
 
-# TEST
-Compare(lpath_1, lpath_2, -6)
