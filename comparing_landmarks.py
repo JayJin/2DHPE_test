df1c3f8f7ad11e783098b0c12963bd2a063b0fd5
JayJin <jvidorca@gmail.com>
2023-10-09 20:32:40 +0900

    Modify code structure in comparing_landmarks.py

--- comparing_landmarks.py
+++ comparing_landmarks.py
@@ -9,42 +9,24 @@ from match_audiosync import MatchSync
 def Compare(lpath_1, lpath_2, sync_difference_seconds):
     diff = sync_difference_seconds
     # load data
+    with open(lpath_1, 'r') as f:
+        data1 = list(csv.reader(f, delimiter=","))
+    with open(lpath_2, 'r') as f:
+        data2 = list(csv.reader(f, delimiter=","))
+
     if diff > 0:
-        with open(lpath_1, 'r') as f:
-            data = list(csv.reader(f, delimiter=","))
-        ldmk_1 = np.array(data)[1+diff:, 1:]
-        r1, c1 = ldmk_1.shape
-        # print('r1, c1:', r1, c1)
-
-        with open(lpath_2, 'r') as f:
-            data = list(csv.reader(f, delimiter=","))
-        ldmk_2 = np.array(data)[1:, 1:]
-        r2, c2 = ldmk_2.shape
-        # print('r2, c2:', r2, c2)
+        ldmk_1 = np.array(data1)[1+diff:, 1:]
+        ldmk_2 = np.array(data2)[1:, 1:]
     elif diff == 0:
-        with open(lpath_1, 'r') as f:
-            data = list(csv.reader(f, delimiter=","))
-        ldmk_1 = np.array(data)[1:, 1:]
-        r1, c1 = ldmk_1.shape
-        # print('r1, c1:', r1, c1)
-
-        with open(lpath_2, 'r') as f:
-            data = list(csv.reader(f, delimiter=","))
-        ldmk_2 = np.array(data)[1:, 1:]
-        r2, c2 = ldmk_2.shape
-        # print('r2, c2:', r2, c2)
-    else:
-        with open(lpath_1, 'r') as f:
-            data1 = list(csv.reader(f, delimiter=","))
         ldmk_1 = np.array(data1)[1:, 1:]
-        r1, c1 = ldmk_1.shape
-        # print('r1, c1:', r1, c1)
-
-        with open(lpath_2, 'r') as f:
-            data2 = list(csv.reader(f, delimiter=","))
+        ldmk_2 = np.array(data2)[1:, 1:]
+    else:
+        ldmk_1 = np.array(data1)[1:, 1:]
         ldmk_2 = np.array(data2)[1+(diff*(-1)+1):, 1:]
-        r2, c2 = ldmk_2.shape
-        # print('r2, c2:', r2, c2)
+
+    r1, c1 = ldmk_1.shape   
+    r2, c2 = ldmk_2.shape
+    print('r1, c1, r2, c2:', r1, c1, r2, c2)
 
     length_val = min(r1, r2)
     print(f"length of landmarks: {length_val}")
@@ -67,13 +49,12 @@ def Compare(lpath_1, lpath_2, sync_difference_seconds):
 
 
 lpath_1 = "D:\JAY\\2DHPE_test\\2dhpe_test\data\landmarks\origin\\202310091739_스모크챌린지.csv"
-lpath_2 = "D:\JAY\\2DHPE_test\\2dhpe_test\data\landmarks\origin\\202310091739_스모크챌린지.csv"
-# lpath_2 = "D:\JAY\\2DHPE_test\\2dhpe_test\data\landmarks\\new\\202310091739_스모크챌린지 Choreo by 바다.csv"
+lpath_2 = "D:\JAY\\2DHPE_test\\2dhpe_test\data\landmarks\\new\\202310091739_스모크챌린지 Choreo by 바다.csv"
+# lpath_2 = "D:\JAY\\2DHPE_test\\2dhpe_test\data\landmarks\origin\\202310091739_스모크챌린지.csv"
 
 vpath_1 = ".\data\\videos\\202310091700_스모크챌린지.mp4"
-vpath_2 = ".\data\\videos\\202310091700_스모크챌린지.mp4"
-# vpath_2 = ".\data\\videos\\202310091701_스모크챌린지 Choreo by 바다.mp4"
-
+vpath_2 = ".\data\\videos\\202310091701_스모크챌린지 Choreo by 바다.mp4"
+# vpath_2 = ".\data\\videos\\202310091700_스모크챌린지.mp4"
 
 sync = MatchSync(vpath_1, vpath_2)
 # print(math.ceil(sync * 10))
